304802883c40a44f37c0f299f8af2dff9668518d
Manoj <kumarayush1911@gmail.com>
2026-04-29 13:37:48 +0530

    add cors

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 import express from "express";
 import cookieparser from "cookie-parser";
+import cors from "cors"
 
 import connectDB from "./src/config/db.js";
 import env from "./src/config/env.js";
 import authRoute from "./src/routes/auth.route.js";
 import imageRouter from "./src/routes/image.upload.routes.js";
+import infoRoute from "./src/routes/student.info.routes.js";
 
 
 const app = express();
+app.use(cors({
+  origin: "http://localhost:3000", // change to your frontend URL
+  credentials: true
+}));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 
 app.use("/api/auth",authRoute);
 app.use("/api/image",imageRouter);
+app.use("/api/student", infoRoute);
 
 app.get("/", async (req, res) => {
   try {
